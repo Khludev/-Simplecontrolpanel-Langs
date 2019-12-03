60d79a3153db0d1ceac94b9303c2540942eb40fe
Khludev <khludev@gmail.com>
2019-12-03 11:42:36 +0200

    Langs

---
+++ ru/lap.php
@@ -0,0 +1,48 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Simplecontrolpanel Language Lines
+    |--------------------------------------------------------------------------
+    */
+    //models.index
+    'advanced_filter' => 'Фильтр',
+    'filter' => 'Фильтровать',
+    'close' => 'Выход',
+    'name' => 'Название',
+    'date' => 'Дата',
+    'please_select' => 'Выберите из списка',
+    'status' => 'Статус',
+    'today' => 'Cегодня',
+    'yesterday' => 'Вчера',
+    'last_7_days' => 'Последние 7 дней',
+
+    'last_30_days' => 'Последние 30 дней',
+    'this_month' => 'В этом месяце',
+    'last_month' => 'В прошлом месяце',
+    //models.datatable.actions
+    'create' => 'Создать',
+    'delete' => 'Удалить',
+    'update' => 'Редактировать',
+    'read' => 'Посмотреть',
+    //models.datatable.seo_action
+    'create_seo' => 'Создать СЕО',
+    'update_seo' => 'Редактировать СЕО',
+    //models.create
+    'save_and_go_back' => 'Сохранить и выйти',
+    'save' => 'Сохранить',
+    //models input fields
+    'id' => 'ID',
+    'created_at' => 'Создан',
+    'updated_at' => 'Обновлено',
+    'created_by' => 'Создан пользователем',
+    'updated_by' => 'Оновив юзер',
+    'seq' => 'Обновлен пользователем',
+    //Other fields start
+    //
+    //Other fields end
+
+
+];
---
+++ uk/lap.php
@@ -0,0 +1,48 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Simplecontrolpanel Language Lines
+    |--------------------------------------------------------------------------
+    */
+    //models.index
+    'advanced_filter' => 'Фільтр',
+    'filter' => 'Фільтрувати',
+    'close' => 'Вихід',
+    'name' => 'Назва',
+    'date' => 'Дата',
+    'please_select' => 'Виберіть зі списку',
+    'status' => 'Статус',
+    'today' => 'Сьогодні',
+    'yesterday' => 'Вчора',
+    'last_7_days' => 'Останні 7 днів',
+
+    'last_30_days' => 'Останні 30 днів',
+    'this_month' => 'Цього місяця',
+    'last_month' => 'Минулого місяця',
+    //models.datatable.actions
+    'create' => 'Створити',
+    'delete' => 'Видалити',
+    'update' => 'Редагувати',
+    'read' => 'Переглянути',
+    //models.datatable.seo_action
+    'create_seo' => 'Створити СЕО',
+    'update_seo' => 'Редагувати СЕО',
+    //models.create
+    'save_and_go_back' => 'Зберегти і вийти',
+    'save' => 'Зберегти',
+    //models input fields
+    'id' => 'ID',
+    'created_at' => 'Створено',
+    'updated_at' => 'Оновлено',
+    'created_by' => 'Створено користувачем',
+    'updated_by' => 'Оновлено користувачем',
+    'seq' => 'Позиція в списку',
+    //Other fields start
+    //
+    //Other fields end
+
+
+];
